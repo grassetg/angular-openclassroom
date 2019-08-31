3d5fc37c8c706457f274b464df551deabcf51bde
Guillaume Grasset <guillaume.grasset@epita.fr>
2019-08-31 18:36:09 +0200

    [NEW] dummy interval observable on the navbar
    that indicates how much time, in minutes, was spent on the site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
-import {Component, OnInit} from '@angular/core';
-import {AppareilService} from './services/appareil.service';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {interval, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
 
-  constructor(private appareilService: AppareilService) {
+  secondes: number;
+  minutes = 0;
+  counterSubcription: Subscription;
+
+  ngOnInit() {
+    const counter = interval(60000);
+    this.counterSubcription = counter.subscribe(
+      (value) => {
+        this.minutes = value + 1;
+      },
+      (error) => {
+        console.log('Uh-oh, an error occured! : ' + error);
+      },
+      () => {
+        console.log('Observable complete!');
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.counterSubcription.unsubscribe();
   }
 
 }
